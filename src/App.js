175eff475079b15ce98656dcdc99e5cc1d5b9215
shikhar13012001 <75368010+shikhar13012001@users.noreply.github.com>
2022-02-09 00:30:20 +0530

    .

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn {process.env.REACT_APP_HEADING}
         </a>
       </header>
     </div>
